8db871b53b2e90f87861a5dd537361741f884c9e
k1kli <karolklimaszewski0@gmail.com>
2019-11-05 21:56:46 +0100

    Added time controller that can slow down timescale of game and made it react to player movement

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public float slowedTimescale = 0.1f;
+    public float normalTimescale = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = slowedTimescale;
+    }
+
+    public void SetNormalTimescale()
+    {
+        Time.timeScale = normalTimescale;
+    }
+    public void SetSlowedTimescale()
+    {
+        Time.timeScale = slowedTimescale;
+    }
+
+}
