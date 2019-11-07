134876aa9aae47c29a2ec84bcbc012b0081c8407
Zackere <wo1001@wp.pl>
2019-11-07 13:49:49 +0100

    Make StationaryCannon follow the player

---
+++ Assets/Enemies/StationaryCannon/StationaryCannon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryCannon : MonoBehaviour {
+    public Transform headTransform;
+    private Transform playerTransform;
+    private readonly float speed = 1;
+
+    private void OnEnable() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        headTransform.LookAt(playerTransform);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        headTransform.LookAt(playerTransform);
+    }
+}
