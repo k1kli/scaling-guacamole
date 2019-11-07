aa299e2434aa74ed152d894bf9931075c404343d
k1kli <karolklimaszewski0@gmail.com>
2019-11-07 12:44:42 +0100

    Setting fixed delta time according to time scale

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -7,19 +7,23 @@ public class TimeController : MonoBehaviour
 {
     public float slowedTimescale = 0.1f;
     public float normalTimescale = 1.0f;
+    private float defaultFixedDeltaTime;
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = slowedTimescale;
+        defaultFixedDeltaTime = Time.fixedDeltaTime;
+        SetSlowedTimescale();
     }
 
     public void SetNormalTimescale()
     {
         Time.timeScale = normalTimescale;
+        Time.fixedDeltaTime = defaultFixedDeltaTime * normalTimescale;
     }
     public void SetSlowedTimescale()
     {
         Time.timeScale = slowedTimescale;
+        Time.fixedDeltaTime = defaultFixedDeltaTime * slowedTimescale;
     }
 
 }
