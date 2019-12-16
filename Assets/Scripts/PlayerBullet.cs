d3bb6a5394ef3b9ae3581dd452a22d91d0b200a1
k1kli <karolklimaszewski0@gmail.com>
2019-12-16 13:54:19 +0100

    Player bullets leave holes in environment

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 public class PlayerBullet : Bullet
 {
     public Transform explosionPrefab;
-    protected override void OnHit()
+    public Transform bulletHolePrefab;
+    protected override void OnHit(Collider other)
     {
-        Transform explosion = Instantiate<Transform>(explosionPrefab, transform);
+        if(other.CompareTag("Environment"))
+        {
+            Debug.Log("hit env");
+            Transform bullet = Instantiate(bulletHolePrefab);
+            Vector3 pos = transform.position - direction*0.2f;
+            Vector3 closest = other.ClosestPoint(pos);
+            bullet.transform.up = Vector3.Normalize(pos-closest);
+            bullet.localScale *= Random.Range(0.7f, 1.3f);
+            bullet.Rotate(0, Random.Range(0, 360.0f), 0);
+            Debug.Log("hit normal" + bullet.transform.up);
+            bullet.localPosition = closest;
+        }
     }
 }
